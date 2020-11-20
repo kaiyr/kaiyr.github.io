dfb95bd7ac40842db15c53e4e95f784db7d9b3e0
kaiyr <43570554+kaiyr@users.noreply.github.com>
2020-11-20 20:32:10 +0500

    fasdeco

--- fasdeco/js/scripts.js
+++ fasdeco/js/scripts.js
@@ -2,35 +2,35 @@ $(document).ready(function() {
 
 	/***************** Waypoints ******************/
 
-	$('.wp1').waypoint(function() {
-		$('.wp1').addClass('animated fadeInUp');
-	}, {
-		offset: '75%'
-	});
-	$('.wp2').waypoint(function() {
-		$('.wp2').addClass('animated fadeInUp');
-	}, {
-		offset: '75%'
-	});
-	$('.wp3').waypoint(function() {
-		$('.wp3').addClass('animated fadeInRight');
-	}, {
-		offset: '75%'
-	});
+	// $('.wp1').waypoint(function() {
+	// 	$('.wp1').addClass('animated fadeInUp');
+	// }, {
+	// 	offset: '75%'
+	// });
+	// $('.wp2').waypoint(function() {
+	// 	$('.wp2').addClass('animated fadeInUp');
+	// }, {
+	// 	offset: '75%'
+	// });
+	// $('.wp3').waypoint(function() {
+	// 	$('.wp3').addClass('animated fadeInRight');
+	// }, {
+	// 	offset: '75%'
+	// });
 
 	/***************** Initiate Flexslider ******************/
-	$('.flexslider').flexslider({
-		animation: "slide"
-	});
+	// $('.flexslider').flexslider({
+	// 	animation: "slide"
+	// });
 
 	/***************** Initiate Fancybox ******************/
 
-	$('.single_image').fancybox({
-		padding: 4,
-	});
+	// $('.single_image').fancybox({
+	// 	padding: 4,
+	// });
 
 	/***************** Tooltips ******************/
-    $('[data-toggle="tooltip"]').tooltip();
+    // $('[data-toggle="tooltip"]').tooltip();
 
 	/***************** Nav Transformicon ******************/
 
